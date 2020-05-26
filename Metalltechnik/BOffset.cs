40bb43bea53bbc31e5246b8cc47fea99042e270b
Tobias Blendware Pescoller <blendware@live.com>
2020-05-26 06:53:01 +0200

    BOffset unimportant change

--- Metalltechnik/BOffset.cs
+++ Metalltechnik/BOffset.cs
@@ -7,6 +7,7 @@ using Multicad.Geometry;
 using Multicad.CustomObjectBase;
 using System.Windows.Forms;
 using Multicad.DatabaseServices.StandardObjects;
+using System.ComponentModel;
 
 namespace Metallwork
 {
@@ -16,84 +17,86 @@ namespace Metallwork
     {
         private Point3d _pnt1 = new Point3d(0, 0, 0);
         private Point3d _pnt2 = new Point3d(100, 100, 0);
+        private double _Height = 20;
 
         public BOffset()
         {
         }
+
+        [DisplayName("Radius")]
+        [Category("Parameter")]
+        public double Radius { get; set; } = 1;
+        [DisplayName("Thickness")]
+        [Category("Parameter")]
+        public double Thickness { get; set; } = 1.5;
+        [DisplayName("Height")]
+        [Category("Parameter")]
+        public double Height
+        {
+            get
+            {
+                return _Height;
+            }
+            set
+            {
+                if (!TryModify()) return;
+                if (value < 0) {
+                    Point3d point = _pnt1;
+                    _pnt1 = _pnt2;
+                    _pnt2 = point;
+
+                    _Height = value * -1;
+                }
+                else
+                {
+                    _Height = value;
+                }
+            }
+        }
+        [DisplayName("Gap")]
+        [Category("Parameter")]
+        public double Gap { get; set; } = 0.2;
+
         public override void OnDraw(GeometryBuilder dc)
         {
             dc.Clear();
             dc.Color = McDbEntity.ByObject;
 
-            McBlockRef test = new McBlockRef();
-            test.BlockName = "Bending Offset";
-            test.InsertPoint = _pnt1;
+            BendingShape shape1 = new BendingShape(_pnt1, _pnt2, Height, Thickness, Gap);
+            //BendingShape shape2 = new BendingShape(_pnt1, _pnt2, Height, Thickness);
 
-            McDocument doc = McDocumentsManager.GetActiveDoc();
-            if (doc == null) return;
-            McObjectId idRef = doc.GetBlock("Bending Offset");
+            Polyline3d poly = new Polyline3d(new List<Point3d>(shape1.Shape));
+            //poly.SetClosed(true);
 
-            ObjGeomRef blockRef = new ObjGeomRef();
-            blockRef.ObjectId = idRef;
-
-            //blockRef.Tfm = Matrix3d.Identity;
-            blockRef.TranslateBy(_pnt1.GetAsVector());
-            EntityGeometry geom = new EntityGeometry(blockRef);
-            dc.DrawGeometry(geom, 1);
-
-            ObjGeomRef blockRef2 = new ObjGeomRef();
-            blockRef2.ObjectId = idRef;
-            blockRef2.TranslateBy(_pnt2.GetAsVector());
-            EntityGeometry geom2 = new EntityGeometry(blockRef2);
-            dc.DrawGeometry(geom2, 3);
+            Polyline3d.VertexDataOfPolyline polyVertecies = poly.Vertices;
+            for (int i = 0; i < polyVertecies.Count; i++)
+            {
+                polyVertecies.MakeFilletAtVertex(i + 1, Radius);
+            }
+            dc.DrawPolyline(poly);
         }
         public override hresult PlaceObject(PlaceFlags lInsertType)
         {
             InputJig jig = new InputJig();
-            List<McObjectId> SelectObjects = jig.SelectObjects("Select Object");
-            //InputResult res = jig.SelectObject("Select a Polyline");
-            //McObjectId id = SelectObjects[0];//.ObjectId;
+            InputResult res = jig.SelectObject("Select Line");
+
+            if (res.Result != InputResult.ResultCode.Normal)
+                return hresult.e_Fail;
+
+            LineSeg3d line = res.Geometry.LineSeg;
+            _pnt1 = line.StartPoint;
+            _pnt2 = line.EndPoint;
+
+            //McObjectId id = res.ObjectId;
             //DbGeometry selection = id.GetObject();
-            if (SelectObjects.Count == 0)
-            {
-                DbEntity.Erase();
-                return hresult.e_Fail;
-            }
-
-            McDocument document = McDocumentsManager.GetActiveDoc();
-            McDocument block = document.CreateBlock("Bending Offset", true);
-
-            foreach (McObjectId obj in SelectObjects)
-            {
-                McDbObject item = obj.GetObject();
-                block.AddObject(item.Clone());
-            }
-            InputResult res = jig.GetPoint("Select first point:");
-            if (res.Result != InputResult.ResultCode.Normal)
-                return hresult.e_Fail;
-            _pnt1 = res.Point;
-            DbEntity.AddToCurrentDocument();
-            //Exclude this from osnap to avoid osnap to itself
-            jig.ExcludeObject(ID);
-            //Monitor mouse move
-            jig.MouseMove = (s, a) => { TryModify(); _pnt2 = a.Point; DbEntity.Update(); };
-
-            res = jig.GetPoint("Select second point:", res.Point);
-            if (res.Result != InputResult.ResultCode.Normal)
-            {
-                DbEntity.Erase();
-                return hresult.e_Fail;
-            }
-            _pnt2 = res.Point;
-            return hresult.s_Ok;
 
             //if (selection.IsKindOf(DbPolyline.TypeID))
             //{
-            //    poly = new Polyline3d(selection.Geometry.Polyline);
+            //    MessageBox.Show("poly");
             //}
             //else if (selection.IsKindOf(DbLine.TypeID))
             //{
-            //    poly = new Polyline3d(selection.Geometry.LineSeg);
+            //    MessageBox.Show("line");
             //}
             //else
             //{
@@ -101,7 +104,8 @@ namespace Metallwork
             //    DbEntity.Erase();
             //    return hresult.e_Fail;
             //}
-            //DbEntity.AddToCurrentDocument();
+            DbEntity.AddToCurrentDocument();
+            return hresult.s_Ok;
         }
         public override void OnTransform(Matrix3d tfm)
         {
@@ -109,7 +113,7 @@ namespace Metallwork
         }
         public override List<Point3d> OnGetGripPoints()
         {
-            List<Multicad.Geometry.Point3d> arr = new List<Point3d>();
+            List<Point3d> arr = new List<Point3d>();
             return arr;
         }
         public override void OnMoveGripPoints(List<int> indexes, Vector3d offset, bool isStretch)
