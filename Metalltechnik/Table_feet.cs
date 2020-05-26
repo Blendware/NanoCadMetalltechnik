5bd50264c5a2e0b553fa5cb213da636ebcc3e4af
Tobias Blendware Pescoller <blendware@live.com>
2020-05-26 06:53:17 +0200

    Table feet side view

--- Metalltechnik/Table_feet.cs
+++ Metalltechnik/Table_feet.cs
@@ -17,6 +17,7 @@ namespace Metallwork
         private Point3d _pnt1 = new Point3d(0,0,0);
         private Point3d _pnt2 = new Point3d(600, 0, 0);
         private Point3d _pnt3 = new Point3d(0, 200, 0);
+        private Point3d _pnt4 = new Point3d(0, 800, 0);
 
         public Table_feet()
         {
@@ -25,6 +26,12 @@ namespace Metallwork
         [DisplayName("Count")]
         [Category("Parameter")]
         public int Count { get; set; } = 5;
+        [DisplayName("Angle1")]
+        [Category("Parameter")]
+        public int Angle1 { get; set; } = 25;
+        [DisplayName("Angle2")]
+        [Category("Parameter")]
+        public int Angle2 { get; set; } = 75;
 
         public override void OnDraw(GeometryBuilder dc)
         {
@@ -40,16 +47,28 @@ namespace Metallwork
             }
             dc.DrawPolyline(poly);
 
-            Polyline3d flat_bar = RotatedRechtangle(100, 15, new Point3d(_pnt1.X+95.7, _pnt1.Y + (_pnt3.Y - _pnt1.Y)/2, 0), 65);
-            Polyline3d flat_bar2 = RotatedRechtangle(100, 15, new Point3d(_pnt2.X - 95.7, _pnt1.Y + (_pnt3.Y - _pnt1.Y) / 2, 0), -65);
+            //Fl 100x15
+            Polyline3d flat_bar = RotatedRechtangle(100, 15, new Point3d(_pnt1.X+95.7, _pnt1.Y + (_pnt3.Y - _pnt1.Y)/2, 0), Angle1);
+            Polyline3d flat_bar2 = RotatedRechtangle(100, 15, new Point3d(_pnt2.X - 95.7, _pnt1.Y + (_pnt3.Y - _pnt1.Y) / 2, 0), -Angle1);
             dc.DrawPolyline(flat_bar);
             dc.DrawPolyline(flat_bar2);
 
+
+            //Side view
+            Point3d pnt4_base = new Point3d(_pnt3.X, _pnt3.Y + 200, 0);
+            dc.DrawPolyline(new Point3d[] { pnt4_base, new Point3d(pnt4_base.X, pnt4_base.Y + 15, 0), new Point3d(pnt4_base.X+100, pnt4_base.Y + 15, 0), new Point3d(pnt4_base.X+100, pnt4_base.Y + 15, 0), new Point3d(pnt4_base.X+100, pnt4_base.Y, 0), pnt4_base });
+            Point3d top_position = TopPosition(Angle2);
+            dc.DrawLine(new Point3d(pnt4_base.X, pnt4_base.Y + 15, 0), top_position);
+            dc.DrawLine(new Point3d(pnt4_base.X+100, pnt4_base.Y + 15, 0), new Point3d(top_position.X+100, top_position.Y,0));
+            //dc.DrawLine(top_position, new Point3d(top_position.X + 100, top_position.Y, 0));
+            double rech_offset = (_pnt1.DistanceTo(_pnt3)-100)/2;
+            dc.DrawPolyline(new Point3d[] { new Point3d(top_position.X - rech_offset, top_position.Y, 0), new Point3d(top_position.X - rech_offset, top_position.Y + 6, 0), new Point3d(top_position.X + 100 + rech_offset, top_position.Y + 6, 0), new Point3d(top_position.X + 100 + rech_offset, top_position.Y + 6, 0), new Point3d(top_position.X + 100 + rech_offset, top_position.Y, 0), new Point3d(top_position.X - rech_offset, top_position.Y, 0) });
+
+            //Holes
             dc.DrawCircle(new Point3d(_pnt1.X + 35, _pnt1.Y + 25, 0), 2.75);
             dc.DrawCircle(new Point3d(_pnt1.X + 35, _pnt3.Y - 25, 0), 2.75);
             dc.DrawCircle(new Point3d(_pnt2.X - 35, _pnt1.Y + 25, 0), 2.75);
             dc.DrawCircle(new Point3d(_pnt2.X - 35, _pnt3.Y - 25, 0), 2.75);
-
             double inbetween = Math.Round((_pnt1.DistanceTo(_pnt2) - 70) / (Count - 1), 4);
             for (int i = 1; i < Count - 1; i++)
             {
@@ -61,6 +80,7 @@ namespace Metallwork
         }
         private Polyline3d RotatedRechtangle(double length, double width, Point3d center_position, double angle)
         {
+            angle = 90 - angle;
             List<Point3d> points = new List<Point3d>();
             points.Add(new Point3d(center_position.X - length / 2, center_position.Y - width / 2, 0).Rotate(center_position, angle));
             points.Add(new Point3d(center_position.X - length / 2, center_position.Y + width / 2, 0).Rotate(center_position, angle));
@@ -70,6 +90,14 @@ namespace Metallwork
             Polyline3d poly = new Polyline3d(points);
             poly.SetClosed(true);
             return poly;
+        }
+        private Point3d TopPosition(double angle)
+        {
+            angle = angle * Math.PI / 180;
+            double height = _pnt4.DistanceTo(new Point3d(_pnt3.X, _pnt3.Y + 200+15, 0))-6;
+            double width = height / Math.Tan(angle);
+
+            return new Point3d(_pnt4.X+width, _pnt4.Y-6, 0);
         }
 
         public static Point3d PolarPoint(Point3d point1, Point3d point2, Point3d point3, double radius)
@@ -98,6 +126,7 @@ namespace Metallwork
                 TryModify();
                 _pnt2 = _pnt2 = new Point3d(a.Point.X, _pnt1.Y, 0);
                 _pnt3 = new Point3d(_pnt1.X, _pnt1.Y + 200, 0);
+                _pnt4 = new Point3d(_pnt1.X, _pnt1.Y + 1000, 0);
                 DbEntity.Update();
             };
 
@@ -109,6 +138,7 @@ namespace Metallwork
             }
             _pnt2 = new Point3d(res.Point.X,_pnt1.Y,0);
             _pnt3 = new Point3d(_pnt1.X, _pnt1.Y + 200, 0);
+            _pnt4 = new Point3d(_pnt1.X, _pnt1.Y + 1000, 0);
             return hresult.s_Ok;
         }
         public override void OnTransform(Matrix3d tfm)
@@ -118,6 +148,7 @@ namespace Metallwork
             _pnt1 = _pnt1.TransformBy(tfm);
             _pnt2 = _pnt2.TransformBy(tfm);
             _pnt3 = _pnt3.TransformBy(tfm);
+            _pnt4 = _pnt4.TransformBy(tfm);
         }
         public override List<Point3d> OnGetGripPoints()
         {
@@ -125,6 +156,7 @@ namespace Metallwork
             arr.Add(_pnt1);
             arr.Add(_pnt2);
             arr.Add(_pnt3);
+            arr.Add(_pnt4);
             return arr;
         }
         public override void OnMoveGripPoints(List<int> indexes, Vector3d offset, bool isStretch)
@@ -136,9 +168,15 @@ namespace Metallwork
                 {
                     _pnt1 = new Point3d(_pnt1.X + offset.X, _pnt1.Y, 0);
                     _pnt3 = new Point3d(_pnt1.X, _pnt3.Y, 0);
+                    _pnt4 = new Point3d(_pnt4.X + offset.X, _pnt4.Y, 0);
                 }
                 else if (indexes[i] == 1) _pnt2 = new Point3d(_pnt2.X + offset.X, _pnt2.Y, 0);
-                else if (indexes[i] == 2) _pnt3 = new Point3d(_pnt3.X, _pnt3.Y + offset.Y, 0);
+                else if (indexes[i] == 2)
+                {
+                    _pnt3 = new Point3d(_pnt3.X, _pnt3.Y + offset.Y, 0);
+                    _pnt4 = new Point3d(_pnt4.X, _pnt4.Y + offset.Y, 0);
+                }
+                else if (indexes[i] == 3) _pnt4 = new Point3d(_pnt1.X, _pnt4.Y + offset.Y, 0);
             }
         }
     }
